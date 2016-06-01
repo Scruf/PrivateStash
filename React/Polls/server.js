d49a2fbec68adf9fefe67f2db1b32c73f01aa9a3
scruf <ekozitski@hotmail.com>
2016-06-01 19:14:54 -0400

    Added React Polls app

--- React/Polls/server.js
+++ React/Polls/server.js
@@ -3,7 +3,8 @@ var fs = require('fs'),
 	express = require('express'),
 	body_parser = require('body-parser'),
 	app = express ();
-
+var COMMENT_FILE = path.join(__dirname,'comments.json');
+console.log(COMMENT_FILE);
 app.set('port',(process.env.PORT || 3000));
 app.use('/',express.static(path.join(__dirname,'public')));
 app.use(body_parser.json());
@@ -14,8 +15,42 @@ app.use(function(req,res,next){
 	next();
 });
 app.get('/',function(req,res){
-	res.send("Hello Cunt");
+	res.sendFile('public/index.html')
 })
+app.get('/api/comments',function(req,res){
+	fs.readFile(COMMENT_FILE,function(err,data){
+		if(err){
+			console.log(err);
+			console.log("Errpr");
+			process.exit(1);
+		}else{
+			res.json(JSON.parse(data));
+		}
+	})
+})
+app.post('/api/comments/',function(req,res){
+	fs.readFile(COMMENT_FILE,function(err,data){
+		if(err)
+		{
+			console.log(err);
+			process.exit(1);
+		}
+		var comments = JSON.parse(data);
+		var new_comments = {
+			id: Date.now(),
+			author: req.body.author,
+			text: req.body.text,
+		};
+		comments.push(new_comments);
+		fs.writeFile(COMMENT_FILE,JSON.stringify(comments,null,4),function(err){
+			if(err){
+				console.log(err);
+				process.exit(1);
+			}
+			res.json(comments);
+		});
+	});
+});
 app.listen(app.get('port'),function(){
 	console.log("listening on port ",app.get('port'));
 })
