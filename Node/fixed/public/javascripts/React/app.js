5d8dc7dddb35aa0bd08882237b339e800db7943c
Scruf <ekozitski@hotmail.com>
2016-06-19 12:44:00 -0700

    Can display list.Mapping ajax call to the list

--- Node/fixed/public/javascripts/React/app.js
+++ Node/fixed/public/javascripts/React/app.js
@@ -1,14 +1,37 @@
+$.ajax({
+	type:'GET',
+	contentType:'application/json',
+	url:'/project/details',
+	success: function(data){
+		console.log(data);
+	}
+})
 var Container = React.createClass({
 	render: function(){
 		return (
 			<div class="container">
-				<ul>
-				</ul>
+				<UlContainer/>
 			</div>
 		)
 	} 
 });
-
+var UlContainer = React.createClass({
+	render: function(){
+		return (
+			<ul>
+				<LiContainer />
+			</ul>
+		)
+	}
+})
+var LiContainer = React.createClass({
+	render: function(){
+		return (
+			<li>{this.props.project_name}  {this.props.project_price}</li>
+			
+		)
+	}
+})
 
 ReactDOM.render(
 	<Container pagename="text" />,
