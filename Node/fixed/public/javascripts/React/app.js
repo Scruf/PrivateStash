2fab875f959cfd5f4ff436e679332940a03ea541
Scruf <ekozitski@hotmail.com>
2016-06-19 13:04:23 -0700

    Can map data from ajax call

--- Node/fixed/public/javascripts/React/app.js
+++ Node/fixed/public/javascripts/React/app.js
@@ -3,37 +3,44 @@ $.ajax({
 	contentType:'application/json',
 	url:'/project/details',
 	success: function(data){
-		console.log(data);
-	}
-})
-var Container = React.createClass({
-	render: function(){
-		return (
-			<div class="container">
-				<UlContainer/>
-			</div>
-		)
-	} 
-});
-var UlContainer = React.createClass({
-	render: function(){
-		return (
-			<ul>
-				<LiContainer />
-			</ul>
-		)
-	}
-})
-var LiContainer = React.createClass({
-	render: function(){
-		return (
-			<li>{this.props.project_name}  {this.props.project_price}</li>
-			
-		)
-	}
-})
+		var data_list = data;
+	
+		var Container = React.createClass({
+		render: function(){
+			return (
+				<div class="container">
+					<UlContainer data={this.props.data}/>
+				</div>
+			)
+		} 
+	});
+	var UlContainer = React.createClass({
+		render: function(){
+			var Ulproject = this.props.data.map(function(project){
 
-ReactDOM.render(
-	<Container pagename="text" />,
-	 document.getElementById('content')
-)
+				return (
+					<LiContainer project_name={project.project_name} key={project._id}>{project.project_name}</LiContainer>
+				);
+			});
+			return (
+				<div className="foo">
+					{Ulproject}
+				</div>
+			)
+			
+		}
+	})
+	var LiContainer = React.createClass({
+		render: function(){
+			return (
+				<li>{this.props.project_name}</li>
+			)
+		}
+	})
+
+	ReactDOM.render(
+		<Container data={data}/>,
+	 	document.getElementById('content')
+		)
+	}
+})
