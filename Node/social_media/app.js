b3cae04c6385adecad1548af8cc4ebab12119c1b
Scruf <ekozitski@hotmail.com>
2016-07-02 09:23:56 -0400

    Updated server set up

--- Node/social_media/app.js
+++ Node/social_media/app.js
@@ -1,7 +1,15 @@
 "use strict"
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const method_override = require('method-override');
+const body_parser = require('body-parser');
 const request = require("request");
+app.use(morgan('dev'))
+app.use(body_parser.urlencoded({'extended':true}));
+app.use(body_parser.json());
+app.use(body_parser.json({type:'application/vdn.api+json'}));
+app.use(method_override('X-HTTP-Method-Override'));
 app.get("/",(req,res,next)=>{
   res.send("Hello World");
   res.end("Request to root was made");
