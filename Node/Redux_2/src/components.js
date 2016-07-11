ed667606a6463cdcc99b01828c87d76bfb1652fd
Scruf <ekozitski@hotmail.com>
2016-07-11 19:56:34 -0400

    Final commit of default todos

--- Node/Redux_2/src/components.js
+++ Node/Redux_2/src/components.js
@@ -1,27 +1,26 @@
 import React from 'react';
 
-
-export function Todo(props){
-	const { todo } = props;
-	if (todo.isDone){
-		return <strike>{todo.text}</strike>
-	}else{
-		return <span>{todo.text}</span>
-	}
+export function Todo(props) {
+  const { todo } = props;
+  if(todo.isDone) {
+    return <strike>{todo.text}</strike>;
+  } else {
+    return <span>{todo.text}</span>;
+  }
 }
-export function ToDoList (props){
-	const {todo} = props;
-	return (
-		<div className="todo">
-			<input type="text" placeholder="Add todo" />
-			  <ul className = "todo__list">
-			  	{todos.map(t=>(
-			  		<li  key={t.id} className="todo_item">
-			  			<Todo todo={t} />
-			  		</li>
-			  	))}
-			  </ul>
-			 </div>
 
-	);
+export function TodoList(props) {
+  const { todos } = props;
+  return (
+    <div className='todo'>
+      <input type='text' placeholder='Add todo' />
+      <ul className='todo__list'>
+        {todos.map(t => (
+          <li key={t.id} className='todo__item'>
+            <Todo todo={t} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
