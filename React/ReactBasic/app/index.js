b7178bc68b2401faaab78a09c43318e3d7ac400b
Scruf <ekozitski@hotmail.com>
2016-07-13 21:32:59 -0400

    Broken React Commit

--- React/ReactBasic/app/index.js
+++ React/ReactBasic/app/index.js
@@ -1,16 +1,53 @@
 "use strict"
+let GITHUB_DATA = {
+	name:"Egor Kozitski",
+	username: "Scruf",
+	image: "https://avatars2.githubusercontent.com/u/7702355?v=3&s=460"
+}
+
 const React = require('react');
 const ReactDom = require('react-dom');
 
 
-let HelloWorld = React.createClass({
-	render: ()=>{
+let profile_pic = React.createClass({
+	render: function(){
+		return <img src={this.props.image} />
+	}
+});
+let profile_url = React.createClass({
+	render: function(){
+		return(
+			<div>
+				<a href={"https://github.com/"+this.props.username }>
+					{this.props.username}
+				</a>
+			</div>
+
+		)
+	}
+});
+let name = React.createClass({
+	render: function(){
 		return (
-			<div> Hello World</div>
+			<div>
+				{this.props.name}
+			</div>
+		)
+	}
+});
+let Avatar = React.createClass({
+	render: function(){
+		return (
+			<div>
+				<profile_pic image= "https://avatars2.githubusercontent.com/u/7702355?v=3&s=460"/>
+				<profile_url username={this.props.user.username}/>
+				<name name={this.props.user.name} />
+			</div>
 		)
 	}
 })
+
 ReactDom.render(
-	<HelloWorld />,
+	<Avatar user={GITHUB_DATA}/>,
 	document.getElementById('app')
 );
