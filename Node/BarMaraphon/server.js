5a2b95bf915a43c0636078295649d8149636aa75
Scruf <ekozitski@hotmail.com>
2016-07-16 16:17:25 -0400

    Initial commit of Bar maraphon

---
+++ Node/BarMaraphon/server.js
@@ -0,0 +1,23 @@
+"use strict"
+const express = require('express'),
+	app = express(),
+	request = require('request'),
+	morgan = require('morgan'),
+	body_parser = require('body-parser'),
+	path = require('path'),
+	method_override = require('method-override'),
+	yelp = require('yelp');
+
+
+
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
+app.use(body_parser.json());
+app.use(body_parser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,'public')));
+app.get('/',(req,res,next)=>{
+	res.render('index.ejs');
+})
+console.log("Listening on port 8000")
+app.listen(8000  || process.env.PORT);
