7060a8853c01e4855ac0c81bf8b7e7c1b612cffc
Scruf <ekozitski@hotmail.com>
2016-07-24 17:54:01 -0400

    C++ initial commit

--- React/ReactBasic/app/containers/ConfirmBattleContainer.js
+++ React/ReactBasic/app/containers/ConfirmBattleContainer.js
@@ -5,13 +5,25 @@ var ConfirmBattleContainer = React.createClass({
     router: React.PropTypes.object.isRequired
   },
   getInitialState: function () {
+  	console.log("Component initial State");
     return {
       isLoading: true,
       playersInfo: [],
     }
   },
+  componentWillReceiveProps: function(){
+  	console.log("component will receive something I do not know");
+  },
+  componentWillUnmount: function(){
+  	console.log("Component Will Unmount Something I do not know");
+  },
+  componentWillMount: function(){
+  	console.log("Component Will Mount");
+  },
+
   componentDidMount: function () {
     var query = this.props.location.query;
+    console.log("Component Did Mount");
     // Fetch info from github then update state
   },
   render: function () {
