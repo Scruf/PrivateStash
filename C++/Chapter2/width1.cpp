9bd134331c56b8cd407fe0d3172d5f1608fb7561
Scruf <ekozitski@hotmail.com>
2016-07-27 21:07:26 -0400

    Partial commit of Chapter 2

---
+++ C++/Chapter2/width1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(){
+	long pop1 = 2425785,pop2=47,pop3=9761;
+	cout<<"LOCATION "<<" POP. "<<endl
+		<<"Porcity "<<pop1<<endl
+		<<"Hightown "<<pop2<<endl
+		<<"Lowville "<<pop3<<endl;
+		return 0;
+}
