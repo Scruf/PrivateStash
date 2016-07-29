80eeb6aa4c44bf15e46ded31de98d33fdde03e83
Scruf <ekozitski@hotmail.com>
2016-07-28 20:22:55 -0400

    Added signed Vart

---
+++ C++/Chapter2/signtest.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int signedVar = 1500000000;
+	unsigned int unsignVar= 150000000;
+	signedVar = (signedVar*2)/3;
+	unsignVar = (unsignVar*2)/3;
+	cout<<"signedVar "<<signedVar<<endl
+		<<"unsignVar "<<unsignVar<<endl;
+	return 0;
+}
