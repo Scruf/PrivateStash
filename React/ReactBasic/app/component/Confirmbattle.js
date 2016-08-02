1ce5f0479a05cd0c7d91d9d74b5ba35fcff20743
Scruf <ekozitski@hotmail.com>
2016-08-01 20:43:58 -0400

    Added React Battle simple UI

--- React/ReactBasic/app/component/Confirmbattle.js
+++ React/ReactBasic/app/component/Confirmbattle.js
@@ -1,6 +1,8 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
 var ReactRouter = require('react-router');
+var UserDetails = require('./UserDetails');
+var UserDetailsWrapper = require('./UserDetailsWrapper')
 var Link = ReactRouter.Link;
 function lol(obj){
 	return <pre>{JSON.stringify(obj,null,' ')}</pre>
@@ -10,15 +12,16 @@ function Confirmbattle(props){
 	return props.isLoading === true ? <p> Loading </p> :<div className="jumbotron col-sm-12 text-center">
 			<h1>Confirm Players</h1>
 				<div className="col-sm-8 col-sm-offset-2">
-					<div className="col-sm-6">
-						<p className="lead"> Player 1</p>
-						{lol(props.playersInfo[0])}
-					</div>
-				<div className="col-sm-6">
-					<p className="lead"> Player 2</p>
-						Player 2 Info
-						{lol(props.playersInfo[1])}
-				</div>
+			
+						<UserDetailsWrapper header="Player 1">
+							<UserDetails info={props.playersInfo[0]} />
+						</UserDetailsWrapper>
+					
+				
+						
+						<UserDetailsWrapper header="Player 2">
+							<UserDetails info={props.playersInfo[0]} />
+						</UserDetailsWrapper>
 				</div>
 			<div className="col-sm-8 sol-sm-offset-2">
 				<div className="col-sm-12">
