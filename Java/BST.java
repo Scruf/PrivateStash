c178e9d6da4d80b08983a9a39b94d540a740783a
Scruf <ekozitski@hotmail.com>
2016-08-10 21:46:57 -0400

    Added BST java implementation

---
+++ Java/BST.java
@@ -0,0 +1,68 @@
+public class BST{
+	private class Node{
+		int data;
+		Node left;
+		Node right;
+		Node(){
+			this.left = null;
+			this.right = null;
+			this.data = 0
+		};
+		Node (int data){
+			this.left = null;
+			this.right = null;
+			this.data = data;
+		}
+	}
+	private Node root;
+	BST(){
+		root=null;
+	};
+	public Node insert(Node node,int data){
+		if(node==null)
+			node = New Node(data);
+		else{
+			if(data<node.left.data){
+				node.left = insert(node.left,data);
+
+			}
+			else
+				node.right = insert(node.right.data);
+		}
+		return node;
+	}
+	public void insert(int data){
+		root = insert(root,data);
+	}
+	private boolean search(Node root,int data){
+		if (root==null)
+			System.out.print("tree is empty");
+		else{
+			boolean found = false;
+			while(root!=null && !found){
+				if(root.data < data)
+					root = root.left;
+				else if(root.data>data)
+					rooot=root.right;
+				else{
+					found = true;
+					break;
+				}
+			}
+			return found;
+		}
+	}
+	public boolean search(int data){
+		return search(root,data);
+	}
+	public void delete(int data){
+		if (root==null)
+			System.out.print("Nothing to insert");
+		else{
+			if()
+		}
+	}
+	public static void main(String []args){
+
+	}
+}
