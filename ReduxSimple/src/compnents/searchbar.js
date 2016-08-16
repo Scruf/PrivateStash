40c1c0d400b6f2bc5b5ea9e0943d86fe36d214ed
Scruf <ekozitski@hotmail.com>
2016-08-16 19:51:14 -0400

    Added State to a searchbarx

--- ReduxSimple/src/compnents/searchbar.js
+++ ReduxSimple/src/compnents/searchbar.js
@@ -4,14 +4,20 @@ import ReactDOM  from 'react-dom';
 class SearchBar extends Component{
 	constructor(props){
 		super(props);
-		this.state = {input_string:' '}
+		this.state = {input_string:''};
 	}
 	render() {
-		return 	<input onChange={onInputChange}/>;
+		return (
+			<div>
+				<input onChange={this.onInputChange.bind(this)}/>;
+				{this.state.input_string}
+			</div>
+		)
 	}
-
 	onInputChange(e){
-		console.log(e.target.value);
+	
+		this.setState({input_string:e.target.value});
+		console.log(this.state.input_string)
 	}
 }
 
