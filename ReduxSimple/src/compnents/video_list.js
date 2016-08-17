32f7301f34c0a0bc0ed192afc96e3fcabbb22783
Scruf <ekozitski@hotmail.com>
2016-08-17 11:47:28 -0400

    Added video List

---
+++ ReduxSimple/src/compnents/video_list.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const VideoList = (props)=>{
+	console.log(props)
+	return (
+		<ul className="col-md-4 list-group">
+			{props.videos.length}
+		</ul>
+	);
+}
+export default VideoList;
--- ReduxSimple/src/index.js
+++ ReduxSimple/src/index.js
@@ -1,12 +1,28 @@
 //Create new Component and this componenet will produce some html
 //Take this comonents and put it on the page (in the DOM)
-import React from 'react';
+import React, {Component} from 'react';
 import SearchBar from './compnents/searchbar';
 import ReactDOM from 'react-dom'
+import VideoList from './compnents/video_list';
+import YoutubeSearch  from 'youtube-api-search'
 const API_KEY = "AIzaSyAhW_KXN86P6cTsP_uOEoQqefG4VVRoSjE";
 
-const App = ()=>{
-	return <div> <SearchBar /> </div>
+class App extends Component {
+	constructor(props){
+		super(props);
+		this.state = {video:[]};
+		YoutubeSearch({key:API_KEY,term:'cats'},(data)=>{
+			this.setState({video:data});
+		});
+	}
+	render(){
+		return (
+			<div>
+				 <SearchBar /> 
+				 <VideoList videos={this.state.video} />
+	 		</div>
+		 );
+	}	
 }
 
 ReactDOM.render(
