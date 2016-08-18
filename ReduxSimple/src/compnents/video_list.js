b1b8851cc6a9738279a2c1f01d031930d74cbcfc
Scruf <ekozitski@hotmail.com>
2016-08-18 14:55:08 -0400

    Added video list ite
    Please enter the commit message for your changes. Lines starting

--- ReduxSimple/src/compnents/video_list.js
+++ ReduxSimple/src/compnents/video_list.js
@@ -1,11 +1,14 @@
 import React from 'react'
-
+import VideoListItem from './video_list_item'
 
 const VideoList = (props)=>{
 	console.log(props)
+	const video_items = props.videos.map((el)=>{
+		return <VideoListItem video={el} />;
+	});
 	return (
 		<ul className="col-md-4 list-group">
-			{props.videos.length}
+			{video_items}
 		</ul>
 	);
 }
---
+++ ReduxSimple/src/compnents/video_list_item.js
@@ -0,0 +1,7 @@
+import React from 'react'
+const VideoListIntem = (props)=>{
+	return (
+		<li>Video</li>
+	)
+}
+export default VideoListIntem;
