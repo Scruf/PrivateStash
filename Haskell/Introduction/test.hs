da9a30547e2fbeb0e242deb0017e4784107c00b7
Scruf <ekozitski@hotmail.com>
2016-11-17 20:17:18 -0500

    Added Haskel babey step 2

---
+++ Haskell/Introduction/test.hs
@@ -0,0 +1,72 @@
+-- Comments and stuf
+{-
+	Stuff
+-}
+import Data.List
+import System.IO
+
+
+--Int -2^63 2^63
+
+-- sumOfNum  = sum  [1..1000]
+
+
+-- modEx =  mod 5 4
+-- modEx2 = 5 `mod` 4
+-- negNum = 5 + (-4)
+
+-- num9  = 9 :: Int
+-- sqrt9  = sqrt (fromIntegral num9)
+
+primeNumbers = [3,5,7,11]
+morePrimes = primeNumbers ++ [13,17,19]
+
+
+
+favNums = 2 : 7 : 21 : 66 :[]
+
+multList = [[3,5,7],[11,13,17]]
+morePrimes2 = 2 : morePrimes
+
+
+lenPrime  = length morePrimes2
+
+
+revPrime = reverse morePrimes2
+isListEmpty = null morePrimes2
+
+secondPrime = morePrimes2 !! 2
+
+firstPrime = head morePrimes2
+lastPrint = last morePrimes2
+primeInit = init morePrimes2
+first3 = take 3 morePrimes2
+
+is7Inlist = 7 `elem` morePrimes2
+maxPrime = maximum morePrimes2
+minPrint = minimum morePrimes2
+
+
+evenList = [2,4..20]
+latterLIst = ['A','B'..'Z']
+
+inifPow10 = [10,20..]
+
+many2s = take 10 (repeat 2)
+listTimes2 = [x*2 | x <-[1,2..10],x*3 <= 50]
+sumOFList  = zipWith (+) [1,2,3,50]
+
+
+listddd = filter (>5) morePrimes2
+evensss = takeWhile (<=20) [2,4..]
+
+
+multOFList = foldl (*) 1 [2,3,4,5]
+multOFLisss =  foldr (+) 2 [4,5,6,7]
+
+pow3 =  [2^n | n <-[1,2..10]]
+
+multTable = [[x * y | y<-[1..10] | x <-[10..20]]]
+
+randTuple = (1, "randTuple")
+bobSmithc= ("BOb Smig",52)
