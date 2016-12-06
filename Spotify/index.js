5981ab6583785b9ebde67877528081f4612f7264
Scruf <ekozitski@hotmail.com>
2016-12-06 07:17:58 -0500

    Addded a broken callback to Spotify

--- Spotify/index.js
+++ Spotify/index.js
@@ -23,13 +23,13 @@ app.use(cookie_parser())
 SpotifyRouter.route('/')
 .get((req,res,next)=>{
 	const state =  Helpers.random_string()
-	
+	console.log(SpotifyCredentials.SpotifyClientId)
 	res.cookie(state,state_key)
 	res.redirect('https://accounts.spotify.com/authorize?'+
 		querystring.stringify({
 			response_type:'code',
 			client_id:SpotifyCredentials.SpotifyClientId,
-			redirect_uri:'localhost:8000/hello',
+			redirect_uri:'https://localhost:8000/hello',
 			state:state
 		})
 	)
@@ -39,7 +39,48 @@ app.use('/',SpotifyRouter)
 
 SpotifyRouter.route('/hello')
 .get((req,res,next)=>{
-	
+	let code = req.query.code || null
+	let state = req.query.state || null
+	let stored_state = req.cookies ? 
+					   req.cookies[state_key] :
+					   null
+	if (state == null || state !=stored_state){
+		res.redirect('/#'+
+			querystring.stringify({
+				error:'state_mismatch'
+			})
+		)
+	}else{
+		res.clearCookie(state_key)
+		let authOptions = {
+			uri:'https://accounts.spotify.com/api/token',
+			form:{
+				code:code,
+				redirect_uri:'https://localhost:8000/hello',
+				grant_type:'authorization_code'
+			},
+			headers:{
+				'Authorization':'Basic'+(new Buffer(SpotifyCredentials.SpotifyClientId+":"+SpotifyCredentials.SpotifyClientSecret).toString('base64'))
+			},
+			json: true
+		};
+		request.post(authOptions, (error,reponse,body)=>{
+			if (err)
+				throw err;
+			else{
+				const access_token = body.access_token,
+					  refresh_token = body.refresh_token;
+				const options = {
+					uri:'https://api.spotify.com/v1/me',
+					headers:{'Authorization':'Bearer'+access_token},
+					json:true
+				}
+				request.get(options,(error,reponse,body)=>{
+					console.log(body)
+				})
+			}
+		})
+	}
 })
 app.use('/hello',SpotifyRouter)
 
