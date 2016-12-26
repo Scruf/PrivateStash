c136f8df7f31a31a781a2589e59d7092a89f7492
Scruf <ekozitski@hotmail.com>
2016-12-25 20:45:35 -0500

    Can scrape the vendors ids:w

--- ScrapyTest/newegg/newegg/spiders/NewEgg.py
+++ ScrapyTest/newegg/newegg/spiders/NewEgg.py
@@ -1,5 +1,6 @@
 from newegg.items import NeweggItem
 from scrapy.spiders import CrawlSpider
+import re
 #//*[@id="search-results"]/div[2]/table
 class NewEgg(CrawlSpider):
 	name = 'NewEgg'
@@ -8,7 +9,20 @@ class NewEgg(CrawlSpider):
 
 
 	def parse(self,response):
+		items = []
 		# selectors = response.css(".additional-sellers > ul > li")
 		# for selector in selectors:
 		# 	print(selector.css('.sellers-list-item::text').extract())
-		print(response.css('div.additional-sellers > ul.sellers-list > li.sellers-list-item > div > div.item-seller > a::attr(href)').extract())
+		# print(response.css('div.additional-sellers > ul.sellers-list > li.sellers-list-item > div > div.item-seller > a::attr(href)').extract())
+		# print(response.css('div.additional-s>ellers > ul.sellers-list > li.sellers-list-item > div > div.item-seller > a::text').extract())
+		for i,selector in enumerate(response.css('div.additional-sellers > ul.sellers-list > li.sellers-list-item > div ')):
+			if i%2 == 0 or i == 0:
+				items.append(str(selector.css('div.has-form > form.askSellerClassForm>#itemNumber').extract_first().replace('"','')))
+		regex = r"(value=\S{14})"
+	
+		for item in items:
+			match = re.finditer(regex, item)
+			for m in match:
+				print(m.group().split("=")[1])
+
+	
